ab4b84a0131fcab7f0461b6852a9db85e864be9e
Stefan Marx <stefan.marx@datadoghq.com>
2020-01-15 08:52:12 +0100

    Added buildAll.sh to build all my Keyboards in one run

---
+++ buildAll.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+make -j6 clean dz60:codingfragments_brownbox
+make -j6 clean dz60:codingfragments_travelbits
+make -j6 clean kbdfans/kbd67/hotswap:marxworld
+make -j6 clean xd75:codingfragments_mainbits
+
